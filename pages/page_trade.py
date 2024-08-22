60f49dcfee31b7b373a29a92d9f4256b7a570fc9
somin <somin.park@musinsa.com>
2024-08-22 17:55:27 +0900

    trading 섹션 타이틀 영역 디스플레이 체크

--- pages/page_trade.py
+++ pages/page_trade.py
@@ -17,3 +17,18 @@ class TradePage(HelperFunctions):
             "trade_product_list_main_info_icon",
         ]
         return self.get_els_not_shown(els)
+
+    def get_updated_sectioin_list(self, list, section_index):
+        els = [raw_el.replace("no", "no"+str(section_index)) for raw_el in list]
+        return els
+
+    @Screenshot()
+    def verify_displays_tit_area(self, section_index):
+        raw_els = [
+            "trade_carousel_no_title",
+            "trade_carousel_no_see_all",
+            "trade_carousel_no_description"
+        ]
+        els = self.get_updated_sectioin_list(raw_els, section_index)
+
+        return self.get_els_not_shown(els)
--- tests/test_trade.py
+++ tests/test_trade.py
@@ -8,6 +8,11 @@ class TestTrade:
     @pytest.mark.usefixtures("go_to_trade")
     def test_trade(self):
         soft_assertions = []
+
+        # 상단 타이틀 영역 디스플레이 체크
         els_not_shown = TradePage().verify_displays_trade()
         if els_not_shown:
             soft_assertions.append(f"The following elements are not shown, {els_not_shown}")
+
+        # new listing 타이틀 영역 디스플레이 체크
+        TradePage().verify_displays_tit_area(section_index=2)
