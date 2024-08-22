07fedda43d613dccf0831c89a0c18aaecf1f8906
somin <somin.park@musinsa.com>
2024-08-22 17:32:27 +0900

    로그인 계정정보 입력 함수 추가

--- pages/page_assets.py
+++ pages/page_assets.py
@@ -1,3 +1,7 @@
+import os
+
+from selenium.common import NoSuchElementException
+
 from common import values
 from common.helper_functions import HelperFunctions
 from common.xpath_converter import get_aid, get_xpath_by_name
@@ -44,3 +48,33 @@ class AssetsPage(HelperFunctions):
             assertions.append(text_els_not_shown)
 
         return assertions
+
+    @Screenshot()
+    def type_credentials(self):
+        if os.environ["PLATFORM"] == "android":
+            email = False
+            pw = False
+            textfield_android = "//android.widget.EditText"
+            els = self.get_elements(textfield_android, find_by=Selector.XPATH)
+            for el in els:
+                try:
+                    email = self.get_child_element(el, get_aid("login_textfield_email"))
+                    el.click()
+                    el.send_keys(os.environ["ID"])
+                except NoSuchElementException:
+                    try:
+                        pw = self.get_child_element(el, get_aid("login_textfield_pw"))
+                        el.click()
+                        el.send_keys(os.environ["PASSWORD"])
+                    except NoSuchElementException:
+                        pass
+        else:
+            email = self.get_element(get_xpath_by_name("login_textfield_email"), find_by=Selector.XPATH)
+            pw = self.get_element(get_xpath_by_name("login_textfield_pw"), find_by=Selector.XPATH)
+            email.click()
+            email.send_keys(os.environ["ID"])
+            pw.click()
+            pw.send_keys(os.environ["PASSWORD"])
+
+        if not email or not pw:
+            raise NoSuchElementException("either email or pw textfield not shown")
